1d0ee66b466f926dddc2e099d6e9599656cbb28c
kresnawan <sanjayakresnawan@gmail.com>
2026-05-27 18:08:04 +0700

    Menambahkan utilitas untuk men-generate kode untuk setiap item, dan menambahkan lahan

--- src/main/java/Lahan.java
+++ src/main/java/Lahan.java
@@ -1,19 +1,29 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import utils.Kode;
 
 public class Lahan {
     int kode;
     double luas;
     double panjang;
     double lebar;
-    Queue<Aktivitas> aktivitas = new LinkedList<Aktivitas>();
+    ArrayList<Aktivitas> aktivitas = new ArrayList<Aktivitas>();
 
     public Lahan(double luas) {
+        this.kode = Kode.generateKode("lahan");
         this.luas = luas;
     }
-    
-    public Lahan(int kode, double panjang, double lebar) {
-        this.kode = kode;
+
+    public Lahan(double panjang, double lebar) {
+        this.kode = Kode.generateKode("lahan");
         this.panjang = panjang;
         this.lebar = lebar;
         this.luas = panjang * lebar;
@@ -24,6 +34,42 @@ public class Lahan {
     }
 
     public void ubah() {
+
+    }
+
+    public static String insertLahan(Lahan lahanBaru) {
+        ArrayList<Lahan> lahan = Lahan.getSemuaLahan();
+
+        lahan.add(lahanBaru);
+        Gson gson = new Gson();
+
+        String json = gson.toJson(lahan);
+
+        return json;
+    }
+
+    public static ArrayList<Lahan> getSemuaLahan() {
+        Path FILE_PATH = Paths.get(System.getProperty("user.home"), ".rotan", "lahan.json");
+        Gson gson = new Gson();
         
+        if (!Files.exists(FILE_PATH)) {
+            return new ArrayList<>();
+        }
+
+        try {
+            String jsonString = Files.readString(FILE_PATH);
+
+            if (jsonString.trim().isEmpty()) {
+                return new ArrayList<>();
+            }
+
+            Type listType = new TypeToken<ArrayList<Lahan>>() {
+            }.getType();
+
+            return gson.fromJson(jsonString, listType);
+
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
     }
 }
--- src/main/java/RoTan.java
+++ src/main/java/RoTan.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -18,6 +19,9 @@ public class RoTan {
 
         System.out.println("Selamat datang di RoTan..!");
         System.out.println("Aplikasi ini dapat membantu kamu buat mencatat pertumbuhan tanaman.");
+
+        String hasil = Lahan.insertLahan(new Lahan(12));
+        System.out.println(hasil);
     }
 
     private void muatData() throws IOException {
@@ -27,16 +31,23 @@ public class RoTan {
         Path lahanFilePath = folderPath.resolve("lahan.json");
         Path aktivitasFilePath = folderPath.resolve("aktivitas.json");
         Path faseFilePath = folderPath.resolve("fase.json");
+        Path autoIncFilePath = folderPath.resolve("auto_inc");
 
-        if (!(Files.exists(lahanFilePath) && Files.exists(aktivitasFilePath) && Files.exists(faseFilePath))) {
+        if (!(Files.exists(lahanFilePath) && Files.exists(aktivitasFilePath) && Files.exists(faseFilePath) && Files.exists(autoIncFilePath))) {
             try {
                 if (!Files.exists(folderPath)) {
                     Files.createDirectories(folderPath);
                 }
 
+                Files.deleteIfExists(lahanFilePath);
+                Files.deleteIfExists(aktivitasFilePath);
+                Files.deleteIfExists(faseFilePath);
+                Files.deleteIfExists(autoIncFilePath);
+
                 Files.createFile(lahanFilePath);
                 Files.createFile(aktivitasFilePath);
                 Files.createFile(faseFilePath);
+                Files.writeString(autoIncFilePath, "0,0,0");
             } catch (IOException e) {
                 throw e;
             }
